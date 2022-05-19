5a1bafc25c38dc4b72ff5cb2b346ce9fc6037a77
Adam Zimny <adamczyk97@op.pl>
2022-05-19 18:36:56 +0200

    Basic stracture of project

---
+++ src/test/java/com/project/LeaugeOfLegendsApp/LeaugeOfLegendsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.LeaugeOfLegendsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaugeOfLegendsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
