9b33a232cda431265a9fff2dfd864f6d59df0cc9
Adam Zimny <adamczyk97@op.pl>
2024-08-30 18:28:34 +0200

    Change javax validation package to jakarta validation package

--- src/main/java/com/project/LeaugeOfLegendsApp/auth/SignupRequest.java
+++ src/main/java/com/project/LeaugeOfLegendsApp/auth/SignupRequest.java
@@ -2,11 +2,12 @@ package com.project.LeaugeOfLegendsApp.auth;
 
 import java.util.Set;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+
+import javax.validation.constraints.Size;
 
 @Data
 public class SignupRequest {
