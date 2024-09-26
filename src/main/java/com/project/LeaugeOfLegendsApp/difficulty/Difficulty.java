ccc82e553661ecde98dec05177e25a54ed5a9850
Adam Zimny <adamczyk97@op.pl>
2024-09-26 21:52:16 +0200

    Change collection name from singular to prural

--- src/main/java/com/project/LeaugeOfLegendsApp/difficulty/Difficulty.java
+++ src/main/java/com/project/LeaugeOfLegendsApp/difficulty/Difficulty.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Document(collection = "difficulty")
+@Document(collection = "difficulties")
 @Data
 @NoArgsConstructor
 public class Difficulty {
